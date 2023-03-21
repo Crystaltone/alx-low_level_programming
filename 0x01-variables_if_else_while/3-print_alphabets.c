523d55cfbb830707f95a7e2bdc6edd52ca37d404
Michael Ogunnaike <mic.ogunnaike@gmail.com>
2023-03-21 17:19:40 +0100

    random number update

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /*
- * main - Prints the alphabet.
+ * Main - Prints the alphabet in lowercase, and then in uppercase
  *
  * Return: Always 0 (Success)
  */
@@ -8,6 +8,7 @@ int main(void)
 {
 	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
+
 	for (i = 0; i < 52; i++)
 	{
 		putchar(alp[i]);
