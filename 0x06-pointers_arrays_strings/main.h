35a4c3dde93ca90e26ae3bd7164706f34f45855e
Michael Ogunnaike <mic.ogunnaike@gmail.com>
2023-04-22 17:29:05 +0100

    function that concatenates two strings again

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,6 +3,9 @@ main.h
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <unistd.h>
+
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
